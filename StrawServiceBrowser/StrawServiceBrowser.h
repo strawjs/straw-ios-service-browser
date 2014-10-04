312f000a97c07eb64fbfd04e370613a3c2175f75
Yoshiya Hinosawa <stibium121@gmail.com>
2014-10-04 19:52:55 +0100

    Initial commit

---
+++ StrawServiceBrowser/StrawServiceBrowser.h
@@ -0,0 +1,13 @@
+//
+//  StrawServiceBrowser.h
+//  StrawServiceBrowser
+//
+//  Created by kt3k on 2014/10/04.
+//  Copyright (c) 2014年 strawjs.org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StrawServiceBrowser : NSObject
+
+@end
---
+++ StrawServiceBrowser/StrawServiceBrowser.m
@@ -0,0 +1,13 @@
+//
+//  StrawServiceBrowser.m
+//  StrawServiceBrowser
+//
+//  Created by kt3k on 2014/10/04.
+//  Copyright (c) 2014年 strawjs.org. All rights reserved.
+//
+
+#import "StrawServiceBrowser.h"
+
+@implementation StrawServiceBrowser
+
+@end
